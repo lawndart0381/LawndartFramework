96f307d8a9c398ac91ae533a126a7cc9b3831364
Magrann, Sean (sm9378) <sm9378@us.att.com>
2020-06-10 14:01:36 -0400

    Added changes to Package.swift

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "LawndartFramework",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "LawndartFramework",
+            type: .dynamic,
             targets: ["LawndartFramework"]),
     ],
     dependencies: [
